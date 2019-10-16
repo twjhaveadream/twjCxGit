f96fcdd1e2a051659c2541bfd4635607b355e9e0
twj <954431056@qq.com>
2019-10-16 14:26:18 +0800

    提交测试数据

---
+++ day1/src/test/people.java
@@ -0,0 +1,8 @@
+package test;
+
+/**
+ * @author TWJ
+ * @version 1.0, created at 2019/10/16 9:09
+ */
+public class people {
+}
---
+++ day1/src/test/student.java
@@ -0,0 +1,8 @@
+package test;
+
+/**
+ * @author TWJ
+ * @version 1.0, created at 2019/10/16 9:08
+ */
+public class student {
+}
